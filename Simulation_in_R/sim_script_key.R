130f5ed9eb1e084b2d9624bc9f1ae04329031246
Josh Knecht <jknecht1@binghamton.edu>
2025-11-04 12:24:11 -0500

    make key and empty script

--- Simulation_in_R/sim_script_key.R
+++ Simulation_in_R/sim_script_key.R
@@ -1,9 +1,11 @@
 # simulate from a Normal(0, 5)
-
+n_obs <- 100
+x <- rnorm(n_obs, mean = 0, sd = 5)
 
 # what does this look like? What is the mean? What is the variance?
-
-
+hist(x)
+mean(x)
+var(x)
 
 # Let's say x causes some other variable (let's call it y) that we are interested in and we
 # want to model this process. (draw a dag on the whiteboard). If we assume that 
@@ -20,16 +22,18 @@
 #
 # let a = .5, b = .5, and sigma = 5. Simulate 100 observations from our model.
 
-
-
+a <- .5
+b <- .5
+sigma <- 5
+fx <- a + b*x
+y <- rnorm(n_obs, fx, sigma)
 
 # plot the relationship between x and y
-
-
+plot(y ~ x)
+abline(a = .5, b = .5, lwd = 2)
 
 # run a linear model 
-
-
+summary(lm(y ~ x))
 
 # we see our a is the intercept, b is the slope on x, and our sigma is the residual standard error.
 
@@ -43,16 +47,18 @@
 # x ~ Exponential(rate = 1/20)
 #
 # lets make a = 3, b = 2 and sigma = 1
-
-
-
+a <- 3
+b <- 2
+sigma <- 3
+x <- rexp(n_obs, 1/20)
+fx <- a + b*x
+y <- rnorm(n_obs, fx, sigma)
 
 # what does our data y look like?
-
-
+hist(y)
 
 # y is clearly not normal, can we use a regular linear model for it?
-
+summary(lm(y ~ x))
 
 
 # Multiple Variables ------------------------------------------------------
@@ -69,13 +75,18 @@
 #
 # Simulate it
 
+z <- rbinom(n_obs, 1, .5)
+x <- rnorm(n_obs)
+a <- 1
+b1 <- 1.5
+b2 <- -.5
+sigma <- 1
+fxz <- a + b1*x + b2*z
+y <- rnorm(n_obs, fxz, sigma)
 
+hist(y)
 
-
-
-
-
-
+summary(lm(y ~ x + z))
 
 
 # Multiple Variables--But with dependence!! -------------------------------
@@ -90,11 +101,13 @@
 #
 # set all the parameters to 1 and simulate it!
 
-
-
-
-
-
+x <- rnorm(n_obs)
+a2 <- b3 <- sigma2 <- 1
+gx <- a2 + b3*x
+z <- rnorm(n_obs, gx, sigma2)
+a <- b1 <- b2 <- sigma <- 1
+fxz <- a + b1*x + b2*z
+y <- rnorm(n_obs, fxz, sigma)
 
 # We know what the true data generating process, but how do we correctly estimate the parameters for this model? Enthralling discussion ensues.
 
@@ -123,10 +136,15 @@ curve(plogis(.5 + .5*x),-10, 10, lwd = 3)
 #
 # a = 1 b = 2
 
+x <- rnorm(n_obs)
+a <- 1
+b <- 2
+fx <- plogis(a + b*x)
+y <- rbinom(n_obs, 1, fx)
 
+plot(y ~ x)
 
-
-
-
+summary(glm(y ~ x, family = binomial))
 
 # how to interpret the output from logistic regression?
+
